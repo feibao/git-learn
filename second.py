2970fb7a00e690a63e8495ef871c49000095e762
feibao <bwffeibao@163.com>
2025-12-11 17:06:58 +0800

    Update second.py
    update remote second.py

--- second.py
+++ second.py
@@ -1,3 +1,4 @@
 print('This is the second test file for git learn')
 print('The second Python test file')
 print('The test file of git practice')
+print('Add this line in the remote github') #在Github侧对mail分支进行了修改，目的是为了同步改内容到本地
